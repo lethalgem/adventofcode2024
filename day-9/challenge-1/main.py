28530c82f73a1c479b10a82c50fb7843db93705c
lethalgem <iwc123@gmail.com>
2024-12-10 14:53:58 -0500

    Complete day 9 part 1

--- day-9/challenge-1/main.py
+++ day-9/challenge-1/main.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 # parse and create the block list from the disk map
-block_list = ""
+initial_list = []
 id = 0
 is_free_space = False
 with open("day-9/challenge-1/input.txt") as file:
@@ -7,23 +9,34 @@ with open("day-9/challenge-1/input.txt") as file:
         # print(char, end="")
         for i in range(int(char)):
             if is_free_space:
-                block_list += "."
+                initial_list.append(-1) # represents '.'
             else:
-                block_list += str(id)
+                initial_list.append(id)
         is_free_space = not is_free_space
         if not is_free_space:
             id += 1
     # print("")
 
-# print(block_list)
+block_list = np.array(initial_list)
+
+def print_block_list(list):
+    for num in list:
+        if num == -1:
+            print(".", end="")
+        else:
+            print(num, end="")
+        print(" ", end="")
+    print("")
+
+# print_block_list(block_list)
 
 # rearrange the block list as appropriate
-for i, char in enumerate(block_list):
-    if char == ".":
+for i, num in enumerate(block_list):
+    if num == -1:
         # see if we're at the end or not
         is_done = True
         for k in range(i, len(block_list) - 1):
-            if block_list[k] != ".":
+            if block_list[k] != -1:
                 is_done = False
 
         if is_done:
@@ -31,29 +44,25 @@ for i, char in enumerate(block_list):
 
         # find last digit from the right
         replacement_offset = 0
-        for j, end_char in enumerate(reversed(block_list)):
-            if end_char != ".":
+        for j, num_from_end in enumerate(reversed(block_list)):
+            if num_from_end != -1:
                 replacement_offset = j
                 break
         
         # perform the swap
-        char_list = list(block_list)
-        char_list[i], char_list[len(block_list) - 1 - replacement_offset] = char_list[len(block_list) - 1 - replacement_offset], char_list[i],
-        block_list = "".join(char_list)
+        print("performing a swap")
+        block_list[i], block_list[len(block_list) - 1 - replacement_offset] = block_list[len(block_list) - 1 - replacement_offset], block_list[i],
         
-    print(block_list)
+# print_block_list(block_list)
+
+print("going to calc checksum")
 
 # calc checksum
 checksum = 0
-for i, char in enumerate(block_list):
-    # convert to digit and see if we're at end of list
-    digit = 0
-    try:
-        digit = int(char)
-    except ValueError:
-        # print("found '.'")
+for i, num in enumerate(block_list):
+    if num == -1:
         break
 
-    checksum += digit * i
+    checksum += num * i
 
 print(checksum)
